5b9d4187346dc8a9fb9bb1712d3636e6810e0fef
Avinesh Tripathi <avineshtripathi1@gmail.com>
2022-08-15 02:23:19 +0530

    tweet data formation step added

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	//"strings"
 	"time"
 )
 
 func main() {
 	var result Response
-	resp, err := http.Get("https://api.github.com/users/AvineshTripathi/events")
+	resp, err := http.Get("https://github.com/AvineshTripathi.atom")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -22,34 +28,68 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	if err := json.Unmarshal(body, &result); err != nil {
+	if err := xml.Unmarshal(body, &result); err != nil {
 		fmt.Println("cannot unmarshal JSON")
-	} 
-	
+	}
+
 	file := checkAndCreateFile()
 
 	defer file.Close()
 
-	_,err2 := file.WriteString(result[0].ID)
-	if err != nil {
-		log.Fatalln(err2)
-	}
+	var data Metadata
+	data.GetTemplate(&result)
 
+	fmt.Println(data)
+	
 	fmt.Println("DONE!!!!!")
 
 }
 
+func (data *Metadata) editFile(file *os.File, result *Response) {
+	data.GetTemplate(result)
+
+	
+}
+
+func (data *Metadata) GetTemplate(result *Response) {
+	var templates []Template
+	for i, newActivity := range result.Entry {
+		if i == 5 {
+			break
+		}
+		var s = strings.Split(newActivity.ID,"/")[1]
+		var id, err = strconv.Atoi(s)
+		 if err != nil {
+			fmt.Println("Error: %v", err)
+		 }
+		event := GetEvent(newActivity.ID)
+		templates = append(templates, Template{id, event, newActivity.Title.Text, newActivity.Link.Href, newActivity.Updated }) 
+	}
+
+	data.TweetDate = "today"
+	data.CurrentTopId = templates[0].ID
+	data.Templates = templates
+}
+
+
+func GetEvent(s string) string {
+	var rgx = regexp.MustCompile(`:(.*?)/`)
+	rs := rgx.FindStringSubmatch(s)
+	return strings.Split(rs[1], ":")[1]
+}
 
 func checkAndCreateFile() *os.File {
-	file, err := os.Create("tweets"+time.Now().Format("2006-02-01")+".txt")
+	file, err := os.Create("tweets" + time.Now().Format("2006-02-01") + ".txt")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	return file 
+	return file
 }
 
 func PrettyPrint(i interface{}) string {
-    s, _ := json.MarshalIndent(i, "", "\t")
-    return string(s)
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
 }
+
+
--- struct.go
+++ struct.go
@@ -1,137 +1,67 @@
 package main
 
-import "time"
+import (
+	"encoding/xml"
+)
 
-type Response []struct {
-	ID    string `json:"id"`
-	Type  string `json:"type"`
-	Actor struct {
-		ID           int    `json:"id"`
-		Login        string `json:"login"`
-		DisplayLogin string `json:"display_login"`
-		GravatarID   string `json:"gravatar_id"`
-		URL          string `json:"url"`
-		AvatarURL    string `json:"avatar_url"`
-	} `json:"actor"`
-	Repo struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"repo"`
-	Payload struct {
-		Action string `json:"action"`
-		Issue  struct {
-			URL           string `json:"url"`
-			RepositoryURL string `json:"repository_url"`
-			LabelsURL     string `json:"labels_url"`
-			CommentsURL   string `json:"comments_url"`
-			EventsURL     string `json:"events_url"`
-			HTMLURL       string `json:"html_url"`
-			ID            int    `json:"id"`
-			NodeID        string `json:"node_id"`
-			Number        int    `json:"number"`
-			Title         string `json:"title"`
-			User          struct {
-				Login             string `json:"login"`
-				ID                int    `json:"id"`
-				NodeID            string `json:"node_id"`
-				AvatarURL         string `json:"avatar_url"`
-				GravatarID        string `json:"gravatar_id"`
-				URL               string `json:"url"`
-				HTMLURL           string `json:"html_url"`
-				FollowersURL      string `json:"followers_url"`
-				FollowingURL      string `json:"following_url"`
-				GistsURL          string `json:"gists_url"`
-				StarredURL        string `json:"starred_url"`
-				SubscriptionsURL  string `json:"subscriptions_url"`
-				OrganizationsURL  string `json:"organizations_url"`
-				ReposURL          string `json:"repos_url"`
-				EventsURL         string `json:"events_url"`
-				ReceivedEventsURL string `json:"received_events_url"`
-				Type              string `json:"type"`
-				SiteAdmin         bool   `json:"site_admin"`
-			} `json:"user"`
-			Labels            []interface{} `json:"labels"`
-			State             string        `json:"state"`
-			Locked            bool          `json:"locked"`
-			Assignee          interface{}   `json:"assignee"`
-			Assignees         []interface{} `json:"assignees"`
-			Milestone         interface{}   `json:"milestone"`
-			Comments          int           `json:"comments"`
-			CreatedAt         time.Time     `json:"created_at"`
-			UpdatedAt         time.Time     `json:"updated_at"`
-			ClosedAt          interface{}   `json:"closed_at"`
-			AuthorAssociation string        `json:"author_association"`
-			ActiveLockReason  interface{}   `json:"active_lock_reason"`
-			Body              string        `json:"body"`
-			Reactions         struct {
-				URL        string `json:"url"`
-				TotalCount int    `json:"total_count"`
-				Num1       int    `json:"+1"`
-				Num10      int    `json:"-1"`
-				Laugh      int    `json:"laugh"`
-				Hooray     int    `json:"hooray"`
-				Confused   int    `json:"confused"`
-				Heart      int    `json:"heart"`
-				Rocket     int    `json:"rocket"`
-				Eyes       int    `json:"eyes"`
-			} `json:"reactions"`
-			TimelineURL           string      `json:"timeline_url"`
-			PerformedViaGithubApp interface{} `json:"performed_via_github_app"`
-			StateReason           interface{} `json:"state_reason"`
-		} `json:"issue"`
-		Comment struct {
-			URL      string `json:"url"`
-			HTMLURL  string `json:"html_url"`
-			IssueURL string `json:"issue_url"`
-			ID       int    `json:"id"`
-			NodeID   string `json:"node_id"`
-			User     struct {
-				Login             string `json:"login"`
-				ID                int    `json:"id"`
-				NodeID            string `json:"node_id"`
-				AvatarURL         string `json:"avatar_url"`
-				GravatarID        string `json:"gravatar_id"`
-				URL               string `json:"url"`
-				HTMLURL           string `json:"html_url"`
-				FollowersURL      string `json:"followers_url"`
-				FollowingURL      string `json:"following_url"`
-				GistsURL          string `json:"gists_url"`
-				StarredURL        string `json:"starred_url"`
-				SubscriptionsURL  string `json:"subscriptions_url"`
-				OrganizationsURL  string `json:"organizations_url"`
-				ReposURL          string `json:"repos_url"`
-				EventsURL         string `json:"events_url"`
-				ReceivedEventsURL string `json:"received_events_url"`
-				Type              string `json:"type"`
-				SiteAdmin         bool   `json:"site_admin"`
-			} `json:"user"`
-			CreatedAt         time.Time `json:"created_at"`
-			UpdatedAt         time.Time `json:"updated_at"`
-			AuthorAssociation string    `json:"author_association"`
-			Body              string    `json:"body"`
-			Reactions         struct {
-				URL        string `json:"url"`
-				TotalCount int    `json:"total_count"`
-				Num1       int    `json:"+1"`
-				Num10      int    `json:"-1"`
-				Laugh      int    `json:"laugh"`
-				Hooray     int    `json:"hooray"`
-				Confused   int    `json:"confused"`
-				Heart      int    `json:"heart"`
-				Rocket     int    `json:"rocket"`
-				Eyes       int    `json:"eyes"`
-			} `json:"reactions"`
-			PerformedViaGithubApp interface{} `json:"performed_via_github_app"`
-		} `json:"comment"`
-	} `json:"payload"`
-	Public    bool      `json:"public"`
-	CreatedAt time.Time `json:"created_at"`
-	Org       struct {
-		ID         int    `json:"id"`
-		Login      string `json:"login"`
-		GravatarID string `json:"gravatar_id"`
-		URL        string `json:"url"`
-		AvatarURL  string `json:"avatar_url"`
-	} `json:"org"`
+type Response struct {
+	XMLName xml.Name `xml:"feed"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Media   string   `xml:"media,attr"`
+	Lang    string   `xml:"lang,attr"`
+	ID      string   `xml:"id"`
+	Link    []struct {
+		Text string `xml:",chardata"`
+		Type string `xml:"type,attr"`
+		Rel  string `xml:"rel,attr"`
+		Href string `xml:"href,attr"`
+	} `xml:"link"`
+	Title   string `xml:"title"`
+	Updated string `xml:"updated"`
+	Entry   []struct {
+		Text      string `xml:",chardata"`
+		ID        string `xml:"id"`
+		Published string `xml:"published"`
+		Updated   string `xml:"updated"`
+		Link      struct {
+			Text string `xml:",chardata"`
+			Type string `xml:"type,attr"`
+			Rel  string `xml:"rel,attr"`
+			Href string `xml:"href,attr"`
+		} `xml:"link"`
+		Title struct {
+			Text string `xml:",chardata"`
+			Type string `xml:"type,attr"`
+		} `xml:"title"`
+		Author struct {
+			Text string `xml:",chardata"`
+			Name string `xml:"name"`
+			URI  string `xml:"uri"`
+		} `xml:"author"`
+		Thumbnail struct {
+			Text   string `xml:",chardata"`
+			Height string `xml:"height,attr"`
+			Width  string `xml:"width,attr"`
+			URL    string `xml:"url,attr"`
+		} `xml:"thumbnail"`
+		Content struct {
+			Text string `xml:",chardata"`
+			Type string `xml:"type,attr"`
+		} `xml:"content"`
+	} `xml:"entry"`
+}
+
+type Metadata struct {
+	TweetDate    string
+	CurrentTopId int
+	Templates    []Template
+}
+
+type Template struct {
+	ID    int
+	Event string
+	Title string
+	URL   string
+	Date  string
 }
